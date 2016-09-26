7811ba9c046a9539831855b1645cec1f8001c826
Prin0516 <105021046@live.asia.edu.tw>
2016-09-26 15:54:54 +0800

    課堂練習+作業
    課堂練習+作業繳交

---
+++ src/hw/hw02_105021046.java
@@ -0,0 +1,21 @@
+package hw;
+
+import java.util.Scanner;
+
+public class hw02_105021046 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int a = scn.nextInt();
+		int b = scn.nextInt();
+		if (a == 1) {
+			System.out.println("標準體重" + (b - 80) * 0.7);
+
+		} else if (a == 2) {
+			System.out.println("標準體重" + (b - 70) * 0.6);
+		}
+
+	}
+
+}
