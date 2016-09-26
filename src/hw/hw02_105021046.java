30c3c1a931297f7bdbb3939b1452707051e7e9c1
Prin0516 <105021046@live.asia.edu.tw>
2016-09-26 15:57:41 +0800

    2

--- src/hw/hw02_105021046.java
+++ src/hw/hw02_105021046.java
@@ -7,6 +7,7 @@ public class hw02_105021046 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入性別(男:1,女:2)身高");
 		int a = scn.nextInt();
 		int b = scn.nextInt();
 		if (a == 1) {
